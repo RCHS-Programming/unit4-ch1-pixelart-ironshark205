f008b14586fc0186b684f1497c66aafbfb76626a
ironshark205 <sagecollett1122@gmail.com>
2019-11-18 08:28:44 -0500

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -2,3 +2,15 @@ function setPixelColor(pixel)
 {
     pixel.style.backgroundColor ="black"; 
 }
+
+var penColor = 'black';
+
+function setPenColor(pen)
+{
+    penColor=pen;
+}
+
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor=penColor; 
+}
